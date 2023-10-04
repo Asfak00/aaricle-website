aa4875a2f9ba1008ab3bc028a8e9455b0c0e7073
Asfak Ahmed <asfakahmed680@gmail.com>
2023-10-04 15:39:05 +0600

    website completed

--- App/Js/app.js
+++ App/Js/app.js
@@ -1,1 +1,6 @@
+const navbar = document.getElementById("mobile_toggle_nav");
+const barBtn = document.getElementById("barBtn");
 
+barBtn.addEventListener("click", () => {
+  navbar.classList.toggle("toggle");
+});
